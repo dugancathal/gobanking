8af1afc13dd60dd969bfb7a7648c6790ea91f683
Mark Douglass <mark.douglass@gmail.com>
2016-05-13 09:58:50 -0600

    RSpec tests wait for services to boot

--- backends/go/src/github.com/dugancathal/gobanking/spec/spec_helper.rb
+++ backends/go/src/github.com/dugancathal/gobanking/spec/spec_helper.rb
@@ -6,4 +6,22 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.before(:suite) do
+    success = 10.times.map.any? do
+      sleep 0.5
+      all_servers_up?
+    end
+    raise 'Unable to start all servers.' unless success
+  end
+end
+
+def all_servers_up?
+  %w(http://products.dev http://bank.dev http://cart.dev/ http://purchase.dev/).all? do |url|
+    begin
+      HTTParty.get(url, timeout: 0.2)
+    rescue Net::OpenTimeout, Errno::ECONNREFUSED
+      return false
+    end
+  end
 end
