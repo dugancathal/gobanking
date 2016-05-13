71f6445436f11f894f94845b7f26db3f3b303bf7
TJ Taylor <dugancathal@gmail.com>
2016-05-13 11:35:54 -0600

    Make system-tests pass and wait for backend boot
    Signed-off-by: Mark Kollasch <mkollasch@pivotal.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,21 @@ require 'httparty'
 require 'rspec'
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    success = 10.times.map.any? do
+      sleep 0.5
+      all_servers_up?
+    end
+    raise 'Unable to start all servers.' unless success
+  end
+end
+
+def all_servers_up?
+  %w(http://products.dev http://bank.dev http://cart.dev/ http://purchase.dev/ http://gateway.dev/).all? do |url|
+    begin
+      HTTParty.get(url, timeout: 0.2)
+    rescue Net::OpenTimeout, Errno::ECONNREFUSED
+      return false
+    end
+  end
 end
