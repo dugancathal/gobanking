88b4286b48e81a65052f5b68ac36c736b55a462e
Josh Rossmeisl and Mark Kollasch <pair+jrossmeisl+mkollasch@pivotal.io>
2016-05-24 13:15:45 -0600

    Make Java bank account ID generation agree with Go's implementation

--- backends/java/components/bank/src/main/java/com/dugancathal/javabanking/BankRepository.java
+++ backends/java/components/bank/src/main/java/com/dugancathal/javabanking/BankRepository.java
@@ -11,7 +11,7 @@ public class BankRepository {
 	private Map<String, Bank> bankMap = new HashMap<>();
 
 	public Bank create() {
-		Bank bank = new Bank(Integer.toString(new Random().nextInt()));
+		Bank bank = new Bank(IdAllocator.getId());
 		bankMap.put(bank.getId(), bank);
 		return bank;
 	}
@@ -19,4 +19,30 @@ public class BankRepository {
 	public Bank find(String id) {
 		return bankMap.get(id);
 	}
+
+	private static class IdAllocator {
+        private static IdAllocator instance = null;
+
+        public static String getId() {
+            return getInstance().generateId();
+        }
+
+        private static IdAllocator getInstance() {
+            if (instance == null) {
+                instance = new IdAllocator();
+            }
+
+            return instance;
+        }
+
+        private IdAllocator() {
+            nextId = 1;
+        }
+
+        private int nextId;
+
+        private String generateId() {
+            return Integer.toString(nextId++);
+        }
+    }
 }
