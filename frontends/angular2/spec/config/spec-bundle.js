23a5f2c0d0db04eb78b2c9c7f579ccf8290061a6
Chris Wang and Tim Butterworth <pair+cwang+tbutterworth@pivotal.io>
2018-01-19 09:36:30 -0700

    Cleaned up the frontend-tests Docker configuration for both react and angular

--- frontends/angular2/spec/config/spec-bundle.js
+++ frontends/angular2/spec/config/spec-bundle.js
@@ -12,7 +12,9 @@ Error.stackTraceLimit = Infinity;
 require('core-js');
 
 require('zone.js/dist/zone.js');
+require('zone.js/dist/sync-test');
 require('zone.js/dist/long-stack-trace-zone.js');
+require('zone.js/dist/proxy.js');
 require('zone.js/dist/jasmine-patch.js');
 
 // RxJS
